c2a214558a57051ec714ea43b9e1795fd38df9ca
VGabriel45 <vgabrielmarian21@gmail.com>
2025-01-31 18:25:02 +0200

    feat: added version command

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ program
     .description('CLI tool to scaffold Web3 SDK projects')
     .version(pkg.version)
     .argument('<project-name>', 'Name of the SDK project')
+    .option('--v', 'Show version of package')
     .option('--typescript', 'Use TypeScript (default: true)', true)
     .option('--git', 'Initialize git repository (default: true)', true)
     .action(async (projectName: string, options) => {
